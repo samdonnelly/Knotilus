be31b5c37172028ccab73b014589e0499b6792ba
Sam <samueldonnelly11@gmail.com>
2025-10-27 15:26:50 -0600

    Renamed folders and updated include paths to match. Added a configuration file for FatFs.

---
+++ config/ffconf.h
@@ -0,0 +1,18 @@
+/**
+ * @file ffconf.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief FatFs configuration 
+ * 
+ * @version 0.1
+ * @date 2025-10-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#ifndef _FFCONF_CONFIG_H_
+#define _FFCONF_CONFIG_H_
+
+#endif   // _FFCONF_CONFIG_H_ 
